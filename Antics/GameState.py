ab85a01488050ec1d4f9227c3c8e9930b46430f0
longbowrocks@gmail.com <longbowrocks@gmail.com@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-02 06:47:51 +0000

    Adding the classes we worked on last Thursday. Some of them are complete, while others are not.

---
+++ Antics/GameState.py
@@ -0,0 +1,30 @@
+class GameState:
+    def __init__(self, inputBoard, inputInventories, inputPhase):
+        self.board = inputBoard
+        self.inventories = inputInventories
+        self.phase = inputPhase
+
+    def getBoard(self):
+        return self.board
+
+    def getInventories(self):
+        return self.inventories
+
+    def getPhase(self):
+        return self.phase
+
+    def applyMove(self, inputMove):
+        fromX = fromLoc.getCoords()[0]
+        fromY = fromLoc.getCoords()[1]
+
+        toX = toLoc.getCoords()[0]
+        toY = toLoc.getCoords()[1]
+        
+        tempAnt = self.board[fromX][fromY].getAnt()
+
+        tempAnt.coords = ToLoc.getCoords()
+        self.board[fromX][fromY].ant = None
+        self.board[toX][toY].ant = tempAnt
+
+    def clone(self):
+        
