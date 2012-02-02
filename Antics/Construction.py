78fd722945ec5351f96e985608a239f81533eb27
longbowrocks@gmail.com <longbowrocks@gmail.com@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-02 06:47:43 +0000

    Adding the classes we worked on last Thursday. Some of them are complete, while others are not.

---
+++ Antics/Construction.py
@@ -0,0 +1,10 @@
+class Construction:
+    def __init__(self, inputId, inputCoords):
+        self.id = inputId
+        self.coords = inputCoords
+        
+    def getId(self):
+        return self.id
+    
+    def getCoords(self):
+        return self.coords
