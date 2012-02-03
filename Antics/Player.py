169d410308caada1763c43be8cd6383cffb737e6
longbowrocks <longbowrocks@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-03 01:13:23 +0000

    Added Player.py.

---
+++ Antics/Player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, inputPlayerId):
+        self.playerId = inputPlayerId
+
+    def getMove(self, currentState):
+        #implemented by students to return their next move
+    
+    def getAttack(self, enemyLocations):
+        #implemented by students to select which enemy ant to attack
