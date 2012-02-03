5da86cd63e3a343dcf9619c5a10678edfa336647
longbowrocks <longbowrocks@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-03 00:50:49 +0000

    Added Location.py.

---
+++ Antics/Location.py
@@ -0,0 +1,20 @@
+class Location:
+	def __init__(self, passable, moveCost, coordinates):
+		self.isPassable = passable
+		self.movementCost = moveCost
+		self.ant = None
+		self.constr = None
+		self.coords = coordinates
+	def getAnt(self):
+		return self.ant
+	def getCoords(self):
+		return self.coords
+	def getConstr(self):
+		return self.constr
+	def getMovementCost(self):
+		return self.movementCost
+	def setAnt(self, addAnt):
+		self.ant = addAnt
+	def setConstr(self, addConstr):
+		self.constr = addConstr  
+
