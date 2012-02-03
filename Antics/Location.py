20d0161803bc44214b9d8d1297501adb9ccb7aa2
longbowrocks <longbowrocks@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-03 00:53:35 +0000

    Got rid of getters and updated to most recent code style.

--- Antics/Location.py
+++ Antics/Location.py
@@ -1,20 +1,7 @@
 class Location:
-	def __init__(self, passable, moveCost, coordinates):
-		self.isPassable = passable
-		self.movementCost = moveCost
+	def __init__(self, inputPassable, inputMoveCost, inputCoordinates):
+		self.isPassable = inputPassable
+		self.movementCost = inputMoveCost
 		self.ant = None
 		self.constr = None
-		self.coords = coordinates
-	def getAnt(self):
-		return self.ant
-	def getCoords(self):
-		return self.coords
-	def getConstr(self):
-		return self.constr
-	def getMovementCost(self):
-		return self.movementCost
-	def setAnt(self, addAnt):
-		self.ant = addAnt
-	def setConstr(self, addConstr):
-		self.constr = addConstr  
-
+		self.coords = inputCoordinates
