d89b0b73ab299749a8ae788afc67ceec7a13dcfb
abi.sundaram12 <abi.sundaram12@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-10 01:24:23 +0000

    Added more comments.

--- Antics/Inventory.py
+++ Antics/Inventory.py
@@ -1,3 +1,12 @@
+##
+#Inventory
+#Description: This class keeps track of the resources at a player's disposal
+#
+#Variables
+#   player - The id of the player that this inventory belongs to
+#   ants - The ants belonging to the player
+#   foodCount - The amount of food that the player has to use
+##
 class Inventory:
 	def init(self, playerId, antArray, food):
 		self.player = playerId
