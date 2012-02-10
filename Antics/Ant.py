25af2254b3ee9ce40d87849bfbb50311a26f07d8
adamkauth <adamkauth@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-10 00:46:19 +0000

    Added documentation for Ant.py.

--- Antics/Ant.py
+++ Antics/Ant.py
@@ -1,3 +1,20 @@
+##
+#Ant
+#Description: This class represents an ant on the board. All information
+#   pertaining to ants that may be required by the game is stored in this
+#   class.
+#
+#Variables:
+#   id - A reference to an Ant that can be manually tracked, so that the
+#       programmer can define ant equality.
+#   coords - An int[] of length 2, representing the Ant's position on the
+#       board.  Positions start at (0, 0) in the upper left and increase down
+#       and to the right.
+#   type - Ants come in all shapes and sizes. Type is an int that indexes into
+#       an array of stats for various ant types.
+#   alive - A boolean representing if the Ant's alive or not.
+#   carrying - A boolean representing if the Ant's carrying food or not.
+##
 class Ant:
     def __init__(self, inputId, inputCoords, inputType):
         self.id = inputId
@@ -6,5 +23,10 @@ class Ant:
         self.alive = True
         self.carrying = False
 
+    ##
+    #die
+    #Description: Does all maintenance necessary for removing an Ant from the
+    #   game.
+    ##
     def die(self):
         self.alive = False
