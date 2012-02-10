a3cd4fb6a5190cdd9d7ee705e3fe8ea763690736
adamkauth <adamkauth@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-10 00:58:24 +0000

    Whoa! Building comments!

--- Antics/Building.py
+++ Antics/Building.py
@@ -1,6 +1,18 @@
+##
+#Building
+#Description: Represents any static object that can be placed on the board
+#   aside from food.
+#
+#Variables:
+#   type - int identifying the type of Building, which allows the game to
+#       decide what to do with it.
+#   player - int equal to the playerId of the player that owns this Building.
+#   captureHealth - int representing the amount of damage this Building can
+#       take before being captured.
+##
 class Building(Construction):
 	def __init__(self,inputId,inputCoords, inputType, inputPlayer, inputHealth):
-                super(Building,self).__init__(inputId,inputCoords)
+        super(Building,self).__init__(inputId,inputCoords)
 		self.type = inputType
 		self.player = inputPlayer
 		self.captureHealth = inputHealth		
