7ba1a3009a3f67ba6fb01598865e551692bb0326
longbowrocks <longbowrocks@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-24 01:57:26 +0000

    UserInterface.py is starting to get debugged so that I can continue development. ant.bmp and grass.bmp are just hanging out in this directory so that I can test UserInterface.py.

--- Antics/UserInterface.py
+++ Antics/UserInterface.py
@@ -8,47 +8,52 @@ from pygame.locals import *
 
 WHITE = (255, 255, 255)
 CELL_SIZE = (10,10)
+BOARD_SIZE = (10,10)
+CELL_SPACING = 10
 
 class UserInterface:
     def __init__(self, inputSize):
         self.screen = pygame.display.set_mode(inputSize)
-    
-    def drawBoard(self, currentState):
-        for row in len(currentState.board):
-            for col in len(currentState.board[row]):
-                currentLoc = currentState.board[row][col]
-                if currentLoc.constr != None:
-                    drawConstruction(currentLoc.constr, (row, col))
-                if currentLoc.ant != None:
-                    drawAnt(currentLoc.ant, (row, col))
-        for key in buttons:
-            drawButton(buttons[key][:2], buttons[2])
-        pygame.display.flip()
-    
+
     def drawConstruction(self, item, position):
-        Xpixel = 10 * (position[1] + 1) + CELL_SIZE[0] * position[1]
-        Ypixel = 10 * (position[0] + 1) + CELL_SIZE[1] * position[0]
-        screen.blit(constructions[item.type], (Xpixel, Ypixel))
+        Xpixel = CELL_SPACING * (position[1] + 1) + CELL_SIZE[0] * position[1]
+        Ypixel = CELL_SPACING * (position[0] + 1) + CELL_SIZE[1] * position[0]
+        self.screen.blit(self.constructions[item.type], (Xpixel, Ypixel))
     
     def drawAnt(self, ant, position):
-        Xpixel = 10 * (position[1] + 1) + CELL_SIZE[0] * position[1]
-        Ypixel = 10 * (position[0] + 1) + CELL_SIZE[1] * position[0]
-        screen.blit(ants[ant.type], (Xpixel, Ypixel))
+        Xpixel = CELL_SPACING * (position[1] + 1) + CELL_SIZE[0] * position[1]
+        Ypixel = CELL_SPACING * (position[0] + 1) + CELL_SIZE[1] * position[0]
+        self.screen.blit(self.ants[ant.type], (Xpixel, Ypixel))
+        
+    def drawBoard(self, currentState):
+        for row in xrange(0, len(currentState.board)):
+            for col in xrange(0, len(currentState.board[row])):
+                currentLoc = currentState.board[row][col]
+                if currentLoc.constr != None:
+                    self.drawConstruction(currentLoc.constr, (row, col))
+                if currentLoc.ant != None:
+                    self.drawAnt(currentLoc.ant, (row, col))
+        for key in self.buttons:
+            pass
+            #drawButton(buttons[key][:2], buttons[2])
+        pygame.display.flip()
     
     def initAssets(self):
         #Load textures as Surfaces.
         self.grass = pygame.image.load("grass.bmp")
-        self.ant = pygame.image.load("ant.bmp")
+        self.ants = []
+        self.ants.append(pygame.image.load("ant.bmp"))
         #Make CELL_SIZE equal to the size of an ant image.
-        CELL_SIZE = (ant.get_width(), ant.get_height())
+        CELL_SIZE = (self.grass.get_width(), self.grass.get_height())
         #Make White transparent (alpha 0) for all textures.
-        grass.set_colorkey(WHITE)
-        ant.set_colorkey(WHITE)
+        self.grass.set_colorkey(WHITE)
+        for ant in self.ants:
+            ant.set_colorkey(WHITE)
         #Set up fonts.
         pygame.font.init()
         self.gameFont = pygame.font.Font(None, 25)
         #Button statistics in order: x, y, buttonState(pressed/released)
-        buttons = {
+        self.buttons = {
         'move':(700,10, 1),
         'wait':(700,60, 1),
         'end':(700,110, 1),
@@ -58,4 +63,15 @@ class UserInterface:
         'load':(700,600, 1),
         'start':(700,650, 1)
         }
-        
+      
+#Stuff that shouldn't exist in the final product.
+#I'm using all this to check my drawboard method.
+import GameState, Location, Inventory, Ant
+a=UserInterface((760,760))
+a.initAssets()
+b = [[ Location.Location(True, 1, (row, col)) for col in xrange(0, BOARD_SIZE[0])] for row in xrange(0, BOARD_SIZE[1])]
+b[0][0].ant = Ant.Ant(0, (0,0), 0, 0)
+c = GameState.GameState(b, "inventories", "phase")
+
+while(True):
+    a.drawBoard(c)
