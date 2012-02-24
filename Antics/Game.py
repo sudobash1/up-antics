82254a3daf936fb93ab242ada6502b916b9c581d
abi.sundaram12@gmail.com <abi.sundaram12@gmail.com@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-24 02:03:51 +0000

    Added psuedo code to Game.py

--- Antics/Game.py
+++ Antics/Game.py
@@ -2,6 +2,9 @@
 #Game
 #Description: Keeps track of game logic and manages the play loop.
 ##
+PLAYER_ONE = 0;
+PLAYER_TWO = 1;
+
 class Game:
 	def __init__(self
 		self.players
@@ -13,15 +16,34 @@ class Game:
 	def runGame(self
 		# initialize board, draw start menu, be ready for player input for game parameters
 		
+		#assign first player
+		
 		# *player clicks start game* enter game loop
+		while !isGameOver(PLAYER_ONE) and !isGameOver(PLAYER_TWO):
+			#check what type first player is
+				#get move(list of locs) from first player until end turn is submitted
+				#If computer player, check validMove 
+				#check if player wants to attack
+				#check isGameOver. If so, break
+			#check what type second player is 
+				#get move(list of locs) from second until end turn is submitted 
+				#If computer player, check ValidMove
+				#check if player wants to attack 
+				#check isGameOver If so, break 
+			
+			
+				
+			
 		
 		# once end game has been reached, display screen "player x wins!" OK/Play Again button
 		
 		
-	def isGameOver(self, inputPlayer):
-		if (self.state.inventories[inputPlayer].queen.isAlive == False) or 
-			(self.state.inventories[(inputPlayer + 1) % 2].antHill.captureHealth == 0) or
-			(self.state.inventories[inputPlayer].foodCount == 11):
+	def isGameOver(self, playerId):
+		opponentId = (playerId + 1) % 2
+		
+		if (self.state.inventories[playerId].queen.isAlive == False) or 
+			(self.state.inventories[opponentId].antHill.captureHealth <= 0) or
+			(self.state.inventories[playerId].foodCount >= 11):
 			return True
 		else:
 			return False
@@ -37,8 +59,6 @@ class Game:
 			(inputMove.toLoc.coords[1] >= 0) and 
 			(inputMove.fromLoc.ant.player == inputPlayer) and
 			(inputMove.toLoc.ant == None) 
-			
-		
 	
 	def isValidAttack(self
 	
