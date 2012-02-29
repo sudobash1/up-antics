4dc9a67b61b7ee3623a6a03c74efc3e4caacc343
longbowrocks@gmail.com <longbowrocks@gmail.com@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-02-29 22:59:57 +0000

    More... Stuff.

--- Antics/UserInterface.py
+++ Antics/UserInterface.py
@@ -7,8 +7,8 @@ import pygame
 from pygame.locals import *
 
 WHITE = (255, 255, 255)
-CELL_SIZE = (10,10)
-BOARD_SIZE = (10,10)
+CELL_SIZE = Rect(0,0,10,10)
+BOARD_SIZE = Rect(0,0,10,10)
 CELL_SPACING = 10
 
 class UserInterface:
@@ -16,16 +16,17 @@ class UserInterface:
         self.screen = pygame.display.set_mode(inputSize)
 
     def drawConstruction(self, item, position):
-        Xpixel = CELL_SPACING * (position[1] + 1) + CELL_SIZE[0] * position[1]
-        Ypixel = CELL_SPACING * (position[0] + 1) + CELL_SIZE[1] * position[0]
+        Xpixel = CELL_SPACING * (position[1] + 1) + CELL_SIZE.width * position[1]
+        Ypixel = CELL_SPACING * (position[0] + 1) + CELL_SIZE.height * position[0]
         self.screen.blit(self.constructions[item.type], (Xpixel, Ypixel))
     
     def drawAnt(self, ant, position):
-        Xpixel = CELL_SPACING * (position[1] + 1) + CELL_SIZE[0] * position[1]
-        Ypixel = CELL_SPACING * (position[0] + 1) + CELL_SIZE[1] * position[0]
+        Xpixel = CELL_SPACING * (position[1] + 1) + CELL_SIZE.width * position[1]
+        Ypixel = CELL_SPACING * (position[0] + 1) + CELL_SIZE.height * position[0]
         self.screen.blit(self.ants[ant.type], (Xpixel, Ypixel))
         
     def drawBoard(self, currentState):
+        self.handleEvents()
         self.screen.fill(WHITE)
         for row in xrange(0, len(currentState.board)):
             for col in xrange(0, len(currentState.board[row])):
@@ -41,12 +42,12 @@ class UserInterface:
     
     def initAssets(self):
         global CELL_SIZE
-        #Load textures as Surfaces.
+        #Load textures as Surfaces. Should convert these surfaces later for optimal speed.
         self.grass = pygame.image.load("grass.bmp")
         self.ants = []
         self.ants.append(pygame.image.load("ant.bmp"))
         #Make CELL_SIZE equal to the size of an ant image.
-        CELL_SIZE = (self.grass.get_width(), self.grass.get_height())
+        CELL_SIZE = self.grass.get_rect()
         #Make White transparent (alpha 0) for all textures.
         self.grass.set_colorkey(WHITE)
         for ant in self.ants:
@@ -65,19 +66,25 @@ class UserInterface:
         'load':(700,600, 1),
         'start':(700,650, 1)
         }
+    
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+            elif event.type == pygame.MOUSEBUTTONUP:
+                pass
       
 #Stuff that shouldn't exist in the final product.
 #I'm using all this to check my drawboard method.
 import GameState, Location, Inventory, Ant
 a=UserInterface((760,760))
 a.initAssets()
-b = [[ Location.Location(True, 1, (row, col)) for col in xrange(0, BOARD_SIZE[0])] for row in xrange(0, BOARD_SIZE[1])]
+b = [[ Location.Location(True, 1, (row, col)) for col in xrange(0, BOARD_SIZE.width)] for row in xrange(0, BOARD_SIZE.height)]
 b[0][0].ant = Ant.Ant(0, (0,0), 0, 0)
 b[1][0].ant = Ant.Ant(0, (1,0), 0, 0)
 c = GameState.GameState(b, "inventories", "phase")
 
 while(True):
     a.drawBoard(c)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.exit()
