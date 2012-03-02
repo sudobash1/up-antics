ccf3895fab5810488ffa0137505152e5b38d357f
adamkauth <adamkauth@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-03-02 00:45:25 +0000

    Changed Move.py to take a list of Locations representing the path to take during the move and removed the endLocation variable

--- Antics/Move.py
+++ Antics/Move.py
@@ -4,9 +4,9 @@
 #
 #Variables:
 #   moveType - This represents the type of move the player has made(moveAnt,build and endTurn)
-#   toLoc - This represents the location of where an Ant is moving to
 #   fromLoc - This represents the location of where Ant is moving from or being built from(this
 #       depends on the moveType)
+#	locList - The list of Locations representing the path to take
 #   unitType - This identifies the type of a unit(only relevant to Moves of type build)
 ##
 class Move:
@@ -17,13 +17,13 @@ class Move:
     #
     #Parameters:
     #   inputMoveType - The type of move the Player is making (int)
-    #   inputToLoc - The Location the Ant is moving to (Location)
     #   inputFromLoc - The Location the Ant is moving from (Location)
-    #   inputUnitType - The type of unit being moved (int)
+	#	inputLocList - A list of Locations representing the path to take (List<Location>)
+    #   inputUnitType - The type of unit being built (int)
     ##
-    def __init__(self, inputMoveType, inputToLoc, inputFromLoc, inputUnitType):
+    def __init__(self, inputMoveType, inputFromLoc, inputLocList,inputUnitType):
         self.moveType = inputMoveType
-        self.toLoc = inputToLoc
         self.fromLoc = inputFromLoc
+		self.locList = inputLocList
         self.unitType = inputUnitType
     
