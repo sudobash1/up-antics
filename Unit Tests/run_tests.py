551b89ee340c1d1f39f79e174e0a97d476033460
abi.sundaram12 <abi.sundaram12@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-03-15 02:17:21 +0000

    Engine for running tests actually works now. I tried it.

--- Unit Tests/run_tests.py
+++ Unit Tests/run_tests.py
@@ -1,21 +1,30 @@
-import os
+import os, sys
 
 directoryContents = os.listdir(os.getcwd())
-unitTestFiles = filter(lamda x: x.startswith('test_') and x.endswith('.py') and x != 'run_tests.py', directoryContents)
+unitTestFiles = filter(lambda x: x.startswith('test_') and x.endswith('.py') and x != 'run_tests.py', directoryContents)
 unitTestModules = [__import__(file.rstrip('.py'), globals(), locals(), [], -1) for file in unitTestFiles]
 
 failures = False
 
+#change current directory to location of Antics files so that they can be imported in their test files
+testDirectory = os.getcwd()
+os.chdir(os.path.join("..", "Antics"))
+anticsDirectory = os.getcwd()
+sys.path.insert(0, anticsDirectory)
+#Run tests
 for i in xrange(0, len(unitTestModules)):
     try:
-        unitTestModules[i].runTest()
+        anticsModule = __import__(unitTestFiles[i].rstrip('.py').lstrip("test_"), globals(), locals(), [], -1)
+        unitTestModules[i].runTest(anticsModule)
     except Exception as e:
         failures = True
         file = open(unitTestFiles[i].rstrip('.py') + '.log', 'w')
-        file.write(e + '\n\n')
+        file.write(str(e) + '\n\n')
         file.write('message: ' + (e.message if e.message != '' else 'No Message'))
         file.close()
         print unitTestFiles[i] + " failed. Check associated logfile in this directory for details."
-    
+#change direectory back to initial value
+os.chdir(testDirectory)
+sys.path.remove(anticsDirectory)
 if not failures:
     print 'All tests were successful!'
