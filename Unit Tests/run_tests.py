11f8f75b876992ab91832d1a4742391aa09345a1
abi.sundaram12 <abi.sundaram12@2887bdcb-7119-2c38-0040-97b772271fcd>
2012-03-16 17:11:16 +0000

    Changed run_tests.py from stripping ".py" from the end of the file to concatenating the file name minus three from the end

--- Unit Tests/run_tests.py
+++ Unit Tests/run_tests.py
@@ -1,8 +1,9 @@
 import os, sys
+import re
 
 directoryContents = os.listdir(os.getcwd())
 unitTestFiles = filter(lambda x: x.startswith('test_') and x.endswith('.py') and x != 'run_tests.py', directoryContents)
-unitTestModules = [__import__(file.rstrip('.py'), globals(), locals(), [], -1) for file in unitTestFiles]
+unitTestModules = [__import__(file[:-3], globals(), locals(), [], -1) for file in unitTestFiles]
 
 failures = False
 
@@ -14,11 +15,13 @@ sys.path.insert(0, anticsDirectory)
 #Run tests
 for i in xrange(0, len(unitTestModules)):
     try:
-        anticsModule = __import__(unitTestFiles[i].rstrip('.py').lstrip("test_"), globals(), locals(), [], -1)
+        anticsModule = __import__(unitTestFiles[i].lstrip("test_")[:-3], globals(), locals(), [], -1)
         unitTestModules[i].runTest(anticsModule)
+        print unitTestFiles[i] + ": This is the file just checked"
+
     except Exception as e:
         failures = True
-        file = open(unitTestFiles[i].rstrip('.py') + '.log', 'w')
+        file = open(unitTestFiles[i][:-3] + '.log', 'w')
         file.write(str(e) + '\n\n')
         file.write('message: ' + (e.message if e.message != '' else 'No Message'))
         file.close()
