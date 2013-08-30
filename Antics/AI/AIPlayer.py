34a369cdad8af5e76b3972ef3ff4735a25d45011
amnuxoll@gmail.com <amnuxoll@gmail.com@2887bdcb-7119-2c38-0040-97b772271fcd>
2013-08-30 22:48:36 +0000

    renamed to "Random" since it is.

--- Antics/AI/AIPlayer.py
+++ Antics/AI/AIPlayer.py
@@ -23,7 +23,7 @@ class AIPlayer(Player):
     #   inputPlayerId - The id to give the new player (int)
     ##
     def __init__(self, inputPlayerId):
-        super(AIPlayer,self).__init__(inputPlayerId, "AI Player 1")
+        super(AIPlayer,self).__init__(inputPlayerId, "Random")
     
     ##
     #getPlacement
